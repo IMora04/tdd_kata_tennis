ec5dbec2df0de0a8e5d70cfff4cb1895b5ee68b3
IMora04 <ignmorper1@alum.us.es>
2026-02-26 19:21:25 +0100

    feat: implement first approach of GameScoreTranslator

--- src/main/java/tennis/GameScoreTranslator.java
+++ src/main/java/tennis/GameScoreTranslator.java
@@ -2,8 +2,39 @@ package tennis;
 
 public class GameScoreTranslator {
 
+    ScoreTranslator scoreTranslator;
+
+    GameScoreTranslator() {
+        this.scoreTranslator = new ScoreTranslator();
+    }
+
     public String translate(Game game) {
-        return "";
+        Integer score1 = game.player1Score;
+        Integer score2 = game.player2Score;
+        boolean scoreIsEqual = score1.equals(score2);
+        Integer biggestScore = Integer.max(score1, score2);
+        String playerWithBiggestScore = score1 > score2 ? "Player 1" : "Player 2";
+        Integer scoreDiff = Math.abs(score1 - score2);
+
+        if(!scoreIsEqual) {
+            if(biggestScore < 4) {
+                return scoreTranslator.translate(score1) + "-" + scoreTranslator.translate(score2);
+            }
+            if(scoreDiff.equals(1)) {
+                return "Advantage " + playerWithBiggestScore;
+            }
+            if(scoreDiff.equals(2)) {
+                return playerWithBiggestScore + " wins the set";
+            }
+            throw new IllegalArgumentException("Set should have already ended");
+        }
+        if(score1 >= 0 && score1 < 3) {
+            return scoreTranslator.translate(score1) + "-" + "All";
+        }
+        if(score1 >= 3) {
+            return "Deuce";
+        }
+        throw new IllegalArgumentException("Scores cannot be negative numbers");
     }
 
 }
