766e6c586e157ff355a9e36906421ed02d230e5b
IMora04 <ignmorper1@alum.us.es>
2026-02-26 19:05:12 +0100

    feat: implement first approach of ScoreTranslator

--- src/main/java/tennis/ScoreTranslator.java
+++ src/main/java/tennis/ScoreTranslator.java
@@ -3,7 +3,19 @@ package tennis;
 public class ScoreTranslator {
 
     public String translate(Integer score) {
-        return "";
+        if(score.equals(0)) {
+            return "Love";
+        }
+        if(score.equals(1)) {
+            return "Fifteen";
+        }
+        if(score.equals(2)) {
+            return "Thirty";
+        }
+        if(score.equals(3)) {
+            return "Forty";
+        }
+        throw new IllegalArgumentException("This score cannot be translated");
     }
 
 }
